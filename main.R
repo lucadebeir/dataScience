1c8de3a0b1eee3654de547d1b35aeec6a5f7aa12
Alexandre Girbal <alexandre.girbal.pro@gmail.com>
2019-12-22 04:17:50 +0100

    Début usages et update fréquence

--- main.R
+++ main.R
@@ -33,7 +33,7 @@ intro <- bdd %>%
   group_by(Q1) %>%
   summarise("Nombre_de_personnes"= n())
 
-#View(intro)
+View(intro)
 
 intro2 <- intro %>%
   arrange(desc(Q1)) %>%
@@ -80,24 +80,122 @@ gr_sum <- gros_rouleurs %>%
 View(gr_sum)
 
 moyenne_km_an <- 17000
-sup <- gr_sum %>% 
+sup_moto <- gr_sum %>% 
   group_by(sommeMoto) %>%
   filter(sommeMoto >= moyenne_km_an)
-inf <- gr_sum %>% 
+inf_moto <- gr_sum %>% 
   group_by(sommeMoto) %>%
   filter(sommeMoto < moyenne_km_an)
 
-View(sup)
-View(inf)
+sup_auto <- gr_sum %>% 
+  group_by(auto) %>%
+  filter(auto >= moyenne_km_an)
+inf_auto <- gr_sum %>% 
+  group_by(auto) %>%
+  filter(auto < moyenne_km_an)
+
+View(sup_auto)
+View(inf_auto)
 
 #TODO: afficher la part de gros rouleurs en moto 
 #les gros rouleurs en voiture,
 #les gros rouleurs en moto qui sont des gros rouleurs en voiture
 #les gros rouleurs en voiture qui sont des gros rouleurs en moto
 #à partir de ces diagrammes et calculs on déduit le reste
+#str(sup_auto)
+
+#resum_gros_rouleurs <- data.frame("rows_bdd" = dim(bdd)[1],
+#                                  "rows_sup_auto" = dim(sup_auto)[1],
+ #                                 "rows_sup_moto" = dim(sup_moto)[1],
+  #                                "rows_inf_auto" = dim(inf_auto)[1],
+   #                               "rows_inf_moto" = dim(inf_moto)[1],
+    #                              "modalité de séparation" = "Moyenne fr: 17000 km")
+
+
+
+effectifs <- data.frame(stat=c("Total étudié","Gros rouleurs en moto","Petits rouleurs en moto","Gros rouleurs en auto","Petits rouleurs en auto"),
+                           effectif=c(dim(bdd)[1],dim(sup_moto)[1],dim(inf_moto)[1],dim(sup_auto)[1],dim(inf_auto)[1]))
+effectifs
+
+pourcentage_gros_rouleurs_fr <- effectifs[-5,][-3,][-1,] %>%
+  mutate(pourcentage = round(effectif / dim(bdd)[1] * 100,2))
+pourcentage_petits_rouleurs_fr <- effectifs[-4,][-2,][-1,] %>%
+  mutate(pourcentage = round(effectif / dim(bdd)[1] * 100,2))
+
+View(pourcentage_gros_rouleurs_fr)
+View(pourcentage_petits_rouleurs_fr)
+
+diag_pourcentage_GR_fr <- 
+  ggplot(data=pourcentage_gros_rouleurs_fr, aes(x=stat, y=pourcentage, fill =factor(stat))) +
+  geom_bar(stat="identity") +
+  scale_fill_brewer(palette="Set1") +
+  labs(fill="Véhicule", 
+       x=NULL, 
+       y="%", 
+       title="Répartition du nombre de gros rouleurs en fonction du véhicule", 
+       caption="Figure 2: Répartition du nombre de gros rouleurs en fonction du véhicule") +
+  geom_text(aes(y = pourcentage / 2, label = pourcentage ), color = "white", size=5,check_overlap = TRUE)+ 
+  theme(legend.position="none")
+diag_pourcentage_GR_fr
+
+diag_pourcentage_PR_fr <- 
+  ggplot(data=pourcentage_petits_rouleurs_fr, aes(x=stat, y=pourcentage, fill =factor(stat))) +
+  geom_bar(stat="identity") +
+  scale_fill_brewer(palette="Set1") +
+  labs(fill="Véhicule", 
+       x=NULL, 
+       y="%", 
+       title="Répartition du nombre de petits rouleurs en fonction du véhicule", 
+       caption="Figure 2: Répartition du nombre de petits rouleurs en fonction du véhicule") +
+  geom_text(aes(y = pourcentage / 2, label = pourcentage ), color = "white", size=5,check_overlap = TRUE) + 
+  theme(legend.position="none")
+diag_pourcentage_PR_fr 
+
+
+
 
 #################################################################################
-#----- Diagramme des gros rouleurs en auto parmis les gros rouleurs en moto-----#
+#-----divison des usages-----#
+usages <- bdd %>%
+  select(Identifiant, usage_auto="Q16 [1]",usage_moto1="...66")
+usages[is.na(usages)] <- 0
 
+to_readable_auto <- function(code){
+  if (code == 1)
+    return ("Personnel")
+  if (code == 2)
+    return ("Domicile-travail")
+  if (code == 3)
+    return ("¨Travail")
+  if (code == 4)
+    return ("Personnel")
+  if (code == 3)
+    return ("¨Personnel")
+  if (code == 4)
+    return ("Personnel")
+  
+}
+to_readable_auto_vector <- Vectorize(to_readable_auto, vectorize.args = "code")
+
+to_readable_moto <- function(code){
+  if (code == 0)
+    return ("Non renseigné")
+  if (code == 1)
+    return ("Personnel")
+  if (code == 2)
+    return ("Domicile-travail")
+  if (code == 3)
+    return ("¨Travail")
+}
+to_readable_moto_vector <- Vectorize(to_readable_moto, vectorize.args = "code")
+
+usages <- usages %>% 
+  mutate(Usage_auto = to_readable_auto_vector(usage_auto)) %>%
+  mutate(Usage_moto = to_readable_moto_vector(usage_moto1))
+
+usages2 <- usages %>%
+  filter(Usage_moto != "Non renseigné")
+
+View(usages2)
 
 
